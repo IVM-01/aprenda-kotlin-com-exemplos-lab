cf604faa695f8db3f4e2fbfa6c479573f003bfc2
IVM-01 <iagovalem@gmail.com>
2024-02-01 17:02:00 -0300

    Correção da tipação de getListMatricula, que retornava um tipo Unit

--- desafio.kt
+++ desafio.kt
@@ -10,7 +10,7 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 
     val inscritos = mutableListOf<Usuario>()
 
-    fun getListMatricula(){
+    fun getListMatricula() : MutableList<Usuario>{
             return inscritos
         }
 
@@ -22,6 +22,14 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 }
 
 fun main() {
+
+    val bkKotlin = Formacao("Backend Kotlin", listOf(
+        ConteudoEducacional("Fundamentos em Kotlin", 3, Nivel.BASICO),
+        ConteudoEducacional("Funções em Kotlin", 3, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("Manipulação de Dados em Kotlin", 3, Nivel.DIFICIL)))
+    ))
+
+    println(bkKotlin.conteudos)
 
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
